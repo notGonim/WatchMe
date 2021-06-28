2495facfb79ddccf147f59952894688a6a79ee34
notGonim <mhmodgonim96@gmail.com>
2021-06-28 04:23:16 +0200

    ADDED the Card Component
    * Added the Card.component && Styled the component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, { useContext } from 'react';
+import FeatureProvider, { FeatureContext } from '../../context/feature/feature.context';
 import {
     Container,
     Group,
@@ -16,16 +17,119 @@ import {
     Item,
     Image,
 } from './styles/card.styles';
+import PropTypes from 'prop-types';
 
 
 
 export default function Card({ children, ...restProps }) {
-    const [showFeature, setShowFeature] = useState(false);
-    const [itemFeature, setItemFeature] = useState({});
-  
+
+
     return (
-      <FeatureContext.Provider value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}>
-        <Container {...restProps}>{children}</Container>
-      </FeatureContext.Provider>
+        <FeatureProvider >
+            <Container {...restProps}>{children}</Container>
+        </FeatureProvider>
     );
-  }
+}
+Card.Group = function CardGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>;
+};
+
+Card.Title = function CardTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+};
+
+Card.SubTitle = function CardSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Card.Text = function CardText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+};
+
+Card.Entities = function CardEntities({ children, ...restProps }) {
+    return <Entities {...restProps}>{children}</Entities>;
+};
+
+Card.Meta = function CardMeta({ children, ...restProps }) {
+    return <Meta {...restProps}>{children}</Meta>;
+};
+
+
+Card.Item = function CardItem({ item, children, ...restProps }) {
+    const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+
+    return (
+        <Item
+            onClick={() => {
+                setItemFeature(item);
+                setShowFeature(true);
+            }}
+            {...restProps}
+        >
+            {children}
+        </Item>
+    );
+};
+
+
+Card.Feature = function CardFeature({ children, category, ...restProps }) {
+    const { showFeature, itemFeature, setShowFeature } = useContext(FeatureContext);
+
+    return showFeature ? (
+        <Feature {...restProps} src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}>
+            <Content>
+                <FeatureTitle>{itemFeature.title}</FeatureTitle>
+                <FeatureText>{itemFeature.description}</FeatureText>
+                <FeatureClose onClick={() => setShowFeature(false)}>
+                    <img src="/images/icons/close.png" alt="Close" />
+                </FeatureClose>
+
+                <Group margin="30px 0" flexDirection="row" alignItems="center">
+                    <Maturity rating={itemFeature.maturity}>{itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}</Maturity>
+                    <FeatureText fontWeight="bold">
+                        {itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1)}
+                    </FeatureText>
+                </Group>
+
+                {children}
+            </Content>
+        </Feature>
+    ) : null;
+};
+
+Card.Feature.propTypes = {
+    children: PropTypes.any,
+    category: PropTypes.any
+};
+
+
+Card.propTypes = {
+    children: PropTypes.any,
+};
+
+Card.propTypes = {
+    children: PropTypes.any,
+};
+
+Card.Group.propTypes = {
+    children: PropTypes.any,
+};
+Card.Title.propTypes = {
+    children: PropTypes.any,
+};
+Card.SubTitle.propTypes = {
+    children: PropTypes.any,
+};
+Card.Text.propTypes = {
+    children: PropTypes.any,
+};
+Card.Entities.propTypes = {
+    children: PropTypes.any,
+};
+Card.Meta.propTypes = {
+    children: PropTypes.any,
+};
+Card.Item.propTypes = {
+    children: PropTypes.any,
+    item: PropTypes.any
+};
